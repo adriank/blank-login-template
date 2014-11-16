f99c3cea9f2813e49199a0eba0e6ca11df35e2f8
Adrian Kalbarczyk <adrian.kalbarczyk@gmail.com>
2014-11-16 15:15:20 +0100

    initial version with full registering and password reseting functionality

---
+++ frontend/js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	//$("."+appData.moduleName).addClass("active")
+
+	ac.onAvailable(".ac-popover",function(){
+		$.each(this,function(){
+			var id=$(this).attr("id")
+			$(this).after("<div id='"+id+"-popover' class='"+id+"' tabindex='-1'></div>")
+			$(this).popover({
+				html:true,
+				placement:"bottom",
+				content:$(this).find(".details").html(),
+				container: "#"+id+"-popover"
+			})
+			var self=$(this)
+			$("#"+id+"-popover").blur(function(e){
+				console.log(e)
+				self.popover('hide')
+			})
+		})
+	})
+
+	$('.tooltip-trigger').tooltip()
+
+	if (innerWidth>750) {}
+
+})
